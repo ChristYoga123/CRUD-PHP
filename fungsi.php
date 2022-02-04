2c105a34299aeac96c8272a723697ef89f233b58
ChristYoga123 <93587043+ChristYoga123@users.noreply.github.com>
2022-02-05 00:22:30 +0700

    Update fungsi.php

--- fungsi.php
+++ fungsi.php
@@ -1,8 +1,7 @@
-<?php
-
+<?php 
 function query($query) {
     // koneksi php ke sql
-    $connection = mysqli_connect("localhost", "root", "", "sqldasar");
+    $connection = mysqli_connect("localhost", "root", "", "shoes");
     // ambil data sql dan disimpan ke variable
     $result = mysqli_query($connection, $query);
     // masukkan data sql ke var rows dengan perulangan
@@ -14,24 +13,66 @@ function query($query) {
     return $rows;
 }
 
-function ubah($method) {
+function tambah($method) {
+    // koneksi php ke mysql
+    $connection = mysqli_connect("localhost", "root", "", "shoes");
+
+    // data inputan dimasukkan ke variable
+    $nama = htmlspecialchars($method["nama_produk"]);
+    $deskripsi = htmlspecialchars($method["deskripsi"]);
+    $harga = htmlspecialchars($method["harga"]);
+    $warna = htmlspecialchars($method["warna"]);
+    $ukuran = htmlspecialchars($method["ukuran"]);
+
+    // upload gambar menggunakan func upload
+    $gambar = upload();
+    if(!$gambar) {
+        return false;
+    }
+
+    // data dimasukkan ke database dengan query
+    $query = "INSERT INTO mahasiswa VALUES('', '$nama', '$deskripsi', '$harga', '$warna', '$ukuran', '$gambar')";
+    mysqli_query($connection, $query);
+    return mysqli_affected_rows($connection);
+}
+
+function hapus($id) {
     $connection = mysqli_connect("localhost", "root", "", "sqldasar");
+    $query = "DELETE FROM mahasiswa WHERE id_produk = $id";
+    $result = mysqli_query($connection, $query);
+    return mysqli_affected_rows($connection);
+}
+
+function ubah($method) {
+    $connection = mysqli_connect("localhost", "root", "", "shoes");
 
     // data inputan dimasukkan ke variable
     $id = $method["id"];
-    $nama = htmlspecialchars($method["Nama"]);
-    $nim = htmlspecialchars($method["NIM"]);
-    $jurusan = htmlspecialchars($method["Jurusan"]);
+    $nama = htmlspecialchars($method["nama_produk"]);
+    $deskripsi = htmlspecialchars($method["deskripsi"]);
+    $harga = htmlspecialchars($method["harga"]);
+    $warna = htmlspecialchars($method["warna"]);
+    $ukuran = htmlspecialchars($method["ukuran"]);
+
     $gambar = upload();
     // query update
-    $query = "UPDATE mahasiswa SET 
-              Nama = '$nama',
-              NIM = '$nim',
-              Jurusan = '$jurusan',
+    $query = "UPDATE produk SET 
+              nama_produk = '$nama',
+              deskripsi = '$deskripsi',
+              harga = '$harga',
+              warna = '$warna',
+              ukuran = '$ukuran',
               Gambar = '$gambar'
-              WHERE No = $id";
+              WHERE id_produk = $id";
     mysqli_query($connection, $query);
     return mysqli_affected_rows($connection);
+}
+
+function cari($keyword) {
+    $connection = mysqli_connect("localhost", "root", "", "sqldasar");
+    $query = "SELECT * FROM mahasiswa WHERE Nama LIKE '%$keyword%' OR NIM LIKE '%$keyword%' OR Jurusan LIKE '%$keyword%'";
+    $result = mysqli_query($connection, $query);
+    return $result;
 }
 
 function upload() {
@@ -76,5 +117,4 @@ function upload() {
     // return namafile sehingga jika kita kembali ke func tambah atau update, namafilenya masuk ke variabel gambar sebelumnya
     return $namaFilebaru;
 }
-
 ?>
